22aea4659423f04839c93e3df4600bfbd17e71e6
seshu31 <pavan.seshu@matrixintel.com>
2023-09-01 17:11:03 +0530

    adds initial logina nd register flow

--- App.tsx
+++ App.tsx
@@ -80,7 +80,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: StatusBar.currentHeight,
+    // marginTop: StatusBar.currentHeight,
   },
 });
 
