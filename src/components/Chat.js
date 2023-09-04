855a5dbf80548806c56095e4be57c3ffc3a5e6f6
unknown <sravs.vadlamanu@gmail.com>
2023-09-04 11:58:18 +0530

    tab screens

---
+++ src/components/Chat.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Chat = () => {
+  return <Text style={{color: 'blue'}}>Chat</Text>;
+};
+
+export default Chat;
---
+++ src/components/Groups.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Groups = () => {
+  return <Text style={{color: 'blue'}}>Groups</Text>;
+};
+
+export default Groups;
