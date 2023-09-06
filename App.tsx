652b62828a6dcdf40fde86ad4d21885abb4b0965
venu <venumakaraju0163@gmail.com>
2023-09-06 12:37:47 +0530

    Static Home screen completed

--- App.tsx
+++ App.tsx
@@ -9,37 +9,11 @@ import Index from './src/components/Index';
 import OTP from './src/components/OTP';
 import ForgotPassword from './src/components/ForgotPassword';
 import EnterPassword from './src/components/EnterPassword';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import Index from './src/components/Index';
-// import OTP from './src/components/OTP';
-// import ForgotPassword from './src/components/ForgotPassword';
-// import EnterPassword from './src/components/EnterPassword';
-// import Notification from './src/components/Notification';
-// import ChatSection from './src/shared/ChatSection';
-// import DetailedArticle from './src/shared/DetailedArticle';
-// import GroupDetailsCard from './src/shared/GroupDetailsCard';
-// import GroupMembers from './src/shared/GroupMembers';
-// import CreateGroup from './src/components/CreateGroup';
-// import GroupRequests from './src/shared/GroupRequests';
-// import SearchBar from './src/components/SearchBar';
-// import Profile from './src/components/Profile';
-// import EditProfile from './src/shared/EditProfile';
-// import UserDetails from './src/shared/UserDetails';
-// import UserExperience from './src/shared/UserExperience';
-// import EditExperience from './src/shared/EditExperience';
-// import UserEducation from './src/shared/UserEducation';
-// import EditEducation from './src/shared/EditEducation';
-// import UserSkill from './src/shared/UserSkill';
-// import EditSkill from './src/shared/EditSkill';
-// import UserProject from './src/shared/UserProject';
-// import EditProject from './src/shared/EditProject';
-// import UserCourse from './src/shared/UserCourse';
-// import EditCourse from './src/shared/EditCourse';
-// import UserPublication from './src/shared/UserPublication';
-// import EditPublication from './src/shared/EditPublication';
-// import ManageNetwork from './src/shared/ManageNetwork';
-// import MessageList from './src/shared/MessageList';
-// import MessageCard from './src/shared/MessageCard';
+import DetailedArticle from './src/shared/DetailedArticle';
+import GroupDetailsCard from './src/shared/GroupDetailsCard.js';
+import CreateGroup from './src/components/CreateGroup.js';
+import Profile from './src/components/Profile.js';
+import theme from './theme';
 
 const Stack = createStackNavigator();
 
@@ -63,23 +37,16 @@ const App = () => {
           <Stack.Screen name="otp-verification" component={OTP} />
           <Stack.Screen name="forgot-password" component={ForgotPassword} />
           <Stack.Screen name="enter-password" component={EnterPassword} />
+          <Stack.Screen name="article" component={DetailedArticle} />
+          <Stack.Screen name="group" component={GroupDetailsCard} />
+          <Stack.Screen name="create-group" component={CreateGroup} />
+          <Stack.Screen
+            name="profile"
+            component={Profile}
+            // initialParams={{loginHandler: loginHandler}}
+          />
         </Stack.Navigator>
       </NavigationContainer>
-      {/* {loggedIn != null ? (
-        <NavigationContainer>
-          <Stack.Navigator>
-            {loggedIn ? (
-              <>
-                <Stack.Screen name="register" component={Register} />
-              </>
-            ) : (
-              <></>
-            )}
-          </Stack.Navigator>
-        </NavigationContainer>
-      ) : (
-        <></>
-      )} */}
     </View>
   );
 };
@@ -87,7 +54,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.colors.white,
   },
 });
 
