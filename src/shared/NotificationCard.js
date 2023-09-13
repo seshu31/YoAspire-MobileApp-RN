4830ba9cda61ee85bc8a3270855a7767d8a235f8
venu <venumakaraju0163@gmail.com>
2023-09-13 10:22:38 +0530

    Added NotificationCard

---
+++ src/shared/NotificationCard.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import normalize from 'react-native-normalize';
+import theme from '../../theme';
+import Moment from 'react-moment';
+
+const NotificationCard = item => {
+  const notificationHandler = () => {
+    console.log(
+      'method: Method, ${backend_url}/feed/useractivity/seen/${item.id}',
+    );
+  };
+
+  return (
+    <TouchableOpacity
+      onPress={notificationHandler}
+      activeOpacity={0.5}
+      style={[
+        styles.notificationBody,
+        {
+          backgroundColor: item.seen === 0 ? '#f5f5f5' : '#fff',
+        },
+      ]}>
+      <View style={styles.notification}>
+        <Image
+          style={styles.notificationImage}
+          source={
+            item.img_file_name
+              ? {uri: item.img_file_name}
+              : require('../../assets/male.png')
+          }
+        />
+        <View style={styles.notificationContent}>
+          <Text style={styles.notificationText}>
+            {item.First_Name} {item.Last_Name} {item.title}
+          </Text>
+          <Moment element={Text} fromNow style={styles.notificationTime}>
+            {item.Timestamp}
+          </Moment>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  notificationBody: {
+    borderBottomWidth: normalize(3),
+    borderColor: theme.colors.grey,
+  },
+  notification: {
+    paddingHorizontal: '5%',
+    paddingVertical: normalize(theme.spacing.large),
+    width: '85%',
+    flexDirection: 'row',
+  },
+  notificationImage: {
+    width: normalize(50),
+    height: normalize(50),
+    borderRadius: normalize(100),
+  },
+  notificationContent: {
+    paddingLeft: '5%',
+  },
+  notificationText: {
+    fontSize: normalize(17),
+  },
+  notificationTime: {
+    paddingTop: normalize(7),
+    color: theme.colors.darkgrey,
+  },
+});
+
+export default NotificationCard;
