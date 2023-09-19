953007fe4adb47c86b760f625f9d99f3237c8210
unknown <sravs.vadlamanu@gmail.com>
2023-09-19 12:49:36 +0530

    added manage my network screen

--- src/staticData.js
+++ src/staticData.js
@@ -185,4 +185,190 @@ const Profiles = [
   },
 ];
 
-export {Profiles};
+const Followees = [
+  {
+    FollowerId: 9,
+    profile: {
+      First_Name: 'follower1',
+      Last_Name: 'sravani',
+      heading: 'heading example',
+      DOB: '2000-04-08T18:30:00.000Z',
+      Title: 'Backend developer',
+      Email: 'sravs.vadlamanu2@gmail.com',
+      phone_no: '9186572345',
+      Location: 'Nuzvid',
+    },
+    experience: [
+      {
+        Company_name: 'Aspire',
+        Role: 'Developer',
+        From: '2020-04-08T18:30:00.000Z',
+      },
+      {
+        Company_name: 'Info',
+        Role: 'Backend developer',
+        From: '2020-04-08T18:30:00.000Z',
+      },
+      {
+        Company_name: 'Aspire2',
+        Role: 'Developer2',
+        From: '2020-04-08T18:30:00.000Z',
+      },
+      {
+        Company_name: 'Info2',
+        Role: 'Backend developer2',
+        From: '2020-04-08T18:30:00.000Z',
+      },
+    ],
+    education: [
+      {
+        college_name: 'RGUKT',
+        From: '2000-04-08T18:30:00.000Z',
+        To: '2000-04-08T18:30:00.000Z',
+      },
+      {
+        college_name: 'RGUKT2',
+        From: '2010-04-08T18:30:00.000Z',
+        To: '2011-04-08T18:30:00.000Z',
+      },
+      {
+        college_name: 'RGUKT3',
+        From: '2011-04-08T18:30:00.000Z',
+        To: '2012-04-08T18:30:00.000Z',
+      },
+      {
+        college_name: 'RGUKT4',
+        From: '2012-04-08T18:30:00.000Z',
+        To: '2013-04-08T18:30:00.000Z',
+      },
+    ],
+    skills: ['HTML', 'CSS', 'python', 'java'],
+    publications: [
+      {
+        title: 'title 1',
+        date: '2022-03-25T18:30:00.000Z',
+      },
+      {
+        title: 'title 2',
+        date: '2022-03-25T18:30:00.000Z',
+      },
+      {
+        title: 'title 3',
+        date: '2022-03-25T18:30:00.000Z',
+      },
+    ],
+    courses: [
+      {
+        title: 'course1',
+      },
+      {
+        title: 'course2',
+      },
+      {
+        title: 'course3',
+      },
+    ],
+    projects: [
+      {
+        title: 'title 1',
+        From: '2020-01-08T18:30:00.000Z',
+        To: '2020-05-08T18:30:00.000Z',
+      },
+      {
+        title: 'title 2',
+        From: '2020-02-08T18:30:00.000Z',
+        To: '2020-04-08T18:30:00.000Z',
+      },
+      {
+        title: 'title 3',
+        From: '2020-04-08T18:30:00.000Z',
+        To: '2020-05-08T18:30:00.000Z',
+      },
+    ],
+  },
+  {
+    FolloweeId: 10,
+    profile: {
+      First_Name: 'followee2',
+      heading: 'heading example1',
+      Last_Name: 'sravani1',
+      DOB: '2000-04-08T18:30:00.000Z',
+      Title: 'frontend developer',
+      Email: 'sravs.vadlamanu3@gmail.com',
+      phone_no: '9186572345',
+      Location: 'vijayawada',
+    },
+    experience: [
+      {
+        Company_name: 'Aspire',
+        Role: 'Developer',
+        From: '2020-04-08T18:30:00.000Z',
+      },
+    ],
+    education: [
+      {
+        college_name: 'RGUKT',
+        From: '2000-04-08T18:30:00.000Z',
+        To: '2000-04-08T18:30:00.000Z',
+      },
+    ],
+    skills: ['C', 'python'],
+  },
+  {
+    FolloweeId: 11,
+    profile: {
+      First_Name: 'followee3',
+      Last_Name: 'sravani2',
+      heading: 'heading example2',
+      DOB: '2000-04-08T18:30:00.000Z',
+      Title: 'fullstack developer',
+      Email: 'sravs.vadlamanu4@gmail.com',
+      phone_no: '9186572345',
+      Location: 'hyderabad',
+    },
+    experience: [
+      {
+        Company_name: 'Aspire',
+        Role: 'Developer',
+        From: '2020-04-08T18:30:00.000Z',
+      },
+    ],
+    education: [
+      {
+        college_name: 'RGUKT',
+        From: '2000-04-08T18:30:00.000Z',
+        To: '2000-04-08T18:30:00.000Z',
+      },
+    ],
+    skills: ['C', 'Java'],
+  },
+  {
+    FolloweeId: 12,
+    profile: {
+      First_Name: 'followee4',
+      Last_Name: 'sravani3',
+      heading: 'heading example3',
+      Title: 'developer',
+      DOB: '2000-04-08T18:30:00.000Z',
+      Email: 'sravs.vadlamanu5@gmail.com',
+      phone_no: '9186572345',
+      Location: 'secundrabad',
+    },
+    experience: [
+      {
+        Company_name: 'Aspire',
+        Role: 'Developer',
+        From: '2020-04-08T18:30:00.000Z',
+      },
+    ],
+    education: [
+      {
+        college_name: 'RGUKT',
+        From: '2000-04-08T18:30:00.000Z',
+        To: '2000-04-08T18:30:00.000Z',
+      },
+    ],
+    skills: ['C', 'python'],
+  },
+];
+export {Profiles, Followees};
